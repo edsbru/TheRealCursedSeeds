ea96db58c434d59c5f8caf1651788af8151ff357
PC-CASA-Edu <a@a.com>
2023-06-08 16:05:54 +0200

    Menu de pausa creado pero bugeado

--- Assets/Assets/Scripts/UII/PauseMenu.cs
+++ Assets/Assets/Scripts/UII/PauseMenu.cs
@@ -7,50 +7,23 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
-    [SerializeField] private GameObject plantZoneObject;
-    private PlantZone plantZone;
     public bool isActive = false;
-
     int currentScene;
+
     // Start is called before the first frame update
     void Start()
     {
-
         currentScene = SceneManager.GetActiveScene().buildIndex;
-
-        if(pauseMenu != null)
-        pauseMenu.SetActive(false);
-
-        if(plantZoneObject != null && currentScene == 1)
-        {
-            plantZone = plantZoneObject.GetComponent<PlantZone>();
-        }
-     
-
+        //if(pauseMenu != null)
+        //    pauseMenu.SetActive(false);    
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (gameObject.name == "pauseMenu" && currentScene == 2 )
+        if (gameObject.name == "pauseMenu" && currentScene == 1 || currentScene == 2)
         {
             if (Input.GetKeyDown(KeyCode.Escape) && !isActive)
-            {
-                Time.timeScale = 0.0f;
-                pauseMenu.SetActive(true);
-                isActive = true;
-            }
-            else if (Input.GetKeyDown(KeyCode.Escape) && isActive)
-            {
-                Time.timeScale = 1.0f;
-                pauseMenu.SetActive(false);
-                isActive = false;
-            }
-        }
-        else if(gameObject.name == "pauseMenu" && currentScene == 1)
-        {
-            if (Input.GetKeyDown(KeyCode.Escape) && !isActive && !plantZone.plantMenuActive)
             {
                 Time.timeScale = 0.0f;
                 pauseMenu.SetActive(true);
@@ -71,16 +44,16 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         isActive = false;
     }
-
+    /*
     public void ChangeScene(string SceneName)
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(SceneName);
     }
-
-    public void ExitGame()
+    */
+    public void BackToMenu()
     {
-        Application.Quit();
+        SceneManager.LoadScene(0);
     }
 
 }
